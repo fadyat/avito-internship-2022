b10c25d8fb87c931ffdf3f9aa37e65962e3dc952
fadyat <fadyat@icloud.com>
2022-11-15 23:03:40 +0300

    chore: remove and add some annotations

--- internal/models/pagination.go
+++ internal/models/pagination.go
@@ -1,7 +1,18 @@
 package models
 
+// Pagination godoc
+// @description: Pagination is a pagination of a list of items.
 type Pagination struct {
-	Page    uint64   `json:"page" query:"page"`
-	PerPage uint64   `json:"per_page" query:"per_page"`
+
+	// @description: Page is a current page of the list.
+	// @example:     1
+	Page uint64 `json:"page" query:"page"`
+
+	// @description: PerPage is a number of items per page.
+	// @example:     10
+	PerPage uint64 `json:"per_page" query:"per_page"`
+
+	// @description: OrderBy is a field containing a list of elements to sort by.
+	// @example:     created_at,amount
 	OrderBy []string `json:"order_by" query:"order_by"`
 }
--- internal/persistence/impl.go
+++ internal/persistence/impl.go
@@ -15,69 +15,27 @@ var (
 )
 
 type IHealthRepo interface {
-
-	// Ping godoc
-	// @description: Ping checks if the database connection is working.
 	Ping() error
 }
 
 type IUserWalletRepo interface {
-
-	// CreateUserWallet godoc
-	// @description: CreateUserWallet creates a new wallet for a user, user_id is passed from outer service
 	CreateUserWallet(w dto.UserWallet) (uint64, error)
-
-	// GetUserWalletByID godoc
-	// @description: GetUserWalletByID returns a wallet of a user by its id
 	GetUserWalletByID(id uint64) (*models.UserWallet, error)
-
-	// GetAllWallets godoc
-	// @description: GetAllWallets returns all wallets of all users
 	GetAllWallets() ([]*models.UserWallet, error)
 }
 
 type ITransactionRepo interface {
-
-	// CreateReplenishment godoc
-	// @description: CreateReplenishment creates a new replenishment transaction
 	CreateReplenishment(tr dto.Transaction) (uint64, error)
-
-	// CreateWithdrawal godoc
-	// @description: CreateWithdrawal creates a new withdrawal transaction
 	CreateWithdrawal(tr dto.Transaction) (uint64, error)
-
-	// CreateReservation godoc
-	// @description: CreateReservation creates a new reservation transaction
 	CreateReservation(tr dto.Reservation) (uint64, error)
-
-	// CreateRelease godoc
-	// @description: CreateRelease creates a new release transaction
 	CreateRelease(tr dto.Reservation) (uint64, error)
-
-	// CancelReservation godoc
-	// @description: CancelReservation cancels a reservation transaction
 	CancelReservation(tr dto.Reservation) (uint64, error)
-
-	// GetUserTransactions godoc
-	// @description: GetUserTransactions returns a paginated result of all transactions of a user
 	GetUserTransactions(userID, page, perPage uint64, orderBy []string) ([]*models.Transaction, error)
-
-	// GetUserTransactionsCount godoc
-	// @description: GetUserTransactionsCount returns the number of all transactions of a user
 	GetUserTransactionsCount(userID uint64) (uint64, error)
 }
 
 type IOuterServiceRepo interface {
-
-	// CreateService godoc
-	// @description: CreateService creates a new service, that uses this microservice
 	CreateService(os dto.OuterService) (uint64, error)
-
-	// GetServiceByID godoc
-	// @description: GetServiceByID returns a service by its id
 	GetServiceByID(id uint64) (*models.OuterService, error)
-
-	// GetAllServices godoc
-	// @description: GetAllServices returns all services, that uses this microservice
 	GetAllServices() ([]*models.OuterService, error)
 }
